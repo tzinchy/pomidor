82f24cbf83227a27d360bf2c2cb350c1f30404b9
tzinchy <dreevxq@gmail.com>
2024-12-08 20:52:14 +0300

    add dockerfile

--- backend/auth_service/app/main.py
+++ backend/auth_service/app/main.py
@@ -4,7 +4,3 @@ from api.v1.api.router import router as auth
 app = FastAPI() 
 
 app.include_router(auth)
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
