f321674950295a8e0511060f21cd216a5895ac03
tzinchy <119239827+tzinchy@users.noreply.github.com>
2025-01-24 13:18:48 +0300

    add cors

--- backend/resettlement_department/app/main.py
+++ backend/resettlement_department/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from api.v1.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(
     router  
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
