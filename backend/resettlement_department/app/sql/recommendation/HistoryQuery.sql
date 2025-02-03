5fd765b0df6f8f532c33cb67d3ba71777abf5c72
Iosif-25IS <n.ngrp05@gmail.com>
2025-02-03 13:48:10 +0300

    connect routers

--- backend/resettlement_department/app/sql/recommendation/HistoryQuery.sql
+++ backend/resettlement_department/app/sql/recommendation/HistoryQuery.sql
@@ -5,3 +5,4 @@ new_house_addresses,
 status_id,
 is_downloaded
 FROM history
+ORDER BY history_id
--- frontend/resettlement_department/src/History/HistoryTable/Cells/ActionsCell.jsx
+++ frontend/resettlement_department/src/History/HistoryTable/Cells/ActionsCell.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-export default function ActionsCell({props}){
-    const value = props['props'];
-
+export default function ActionsCell(props){
+    const value = props['props']
+    console.log('ACTION ', value);
     return (
-        <td className="p-2 font-normal whitespace-nowrap">
+        <td className="p-2 font-normal ">
             <div className="flex w-full flex-row items-center justify-start gap-1">
-                <div className="flex flex-1 justify-around truncate">
-                        <button className="px-6 py-2 bg-gray-400 rounded text-white">Скачать</button>
-                        <button className="px-6 py-2 bg-gray-400 rounded text-white">Одобрить</button>
-                        <button className="px-6 py-2 bg-gray-400 rounded text-white">Отменить</button>
+                <div className="flex flex-1 justify-around">
+                        <Button name='Скачать'/>
+                        {value.status_id==1 ? (<Button name='Одобрено' isDisabled={true} />) : (<Button name='Одобрить' />)}
+                        {value.status_id==1 ? value.is_downloaded ? <Button name={'Контейнер загружен'} isDisabled={true} /> : <Button name={'Загрузить контейнер'}/> : (<Button name={'Отменить'} />)}
                 </div>
             </div>
         </td>
+    )
+}
+
+function Button({ name, func=null, isDisabled }){
+    return (
+        <button className={`px-6 py-2 bg-gray-400 rounded text-white min-w-[150px] max-w-[150px] ${isDisabled ? 'opacity-50' : ''}`} disabled={isDisabled} onClick={func}>{name}</button>
     )
 }
