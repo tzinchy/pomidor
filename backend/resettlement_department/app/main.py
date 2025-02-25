b6862f6928b21a1941ff1074a63c30d899bf04df
aleksandr andreev <macbook@MacBookPro.lan>
2025-02-25 08:37:30 +0300

    update methods

--- backend/resettlement_department/app/main.py
+++ backend/resettlement_department/app/main.py
@@ -1,22 +1,18 @@
-#from fastapi_cache import FastAPICache
 from fastapi import FastAPI
-
-#from fastapi_cache.backends.inmemory import InMemoryBackend
 from fastapi.middleware.cors import CORSMiddleware
 from api.v1.router import router
 
-#FastAPICache.init(InMemoryBackend())
-
+# Инициализация FastAPI приложения
 app = FastAPI()
+
+# Настройка CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Явное указание методов,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD", "TRACE", "CONNECT"],
     allow_headers=["*"],
 )
 
-app.include_router(
-    router  
-)
-
+# Подключение роутера
+app.include_router(router)
