a877be3490aa5e7404fad67254dc176e1fb74305
tapo44ek <viktooooor2001@gmail.com>
2025-04-09 17:48:25 +0300

    1

--- backend/resettlement_department/app/kostyl.py
+++ backend/resettlement_department/app/kostyl.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    df = pd.read_excel('/Users/viktor/Desktop/programs/RSM/export_new_aparts.xlsx')
+    df = pd.read_excel('/Users/viktor/Desktop/programs/RSM/trusted_resurs.xlsx')
     result = insert_data_to_new_apart(df)
     print(result)
