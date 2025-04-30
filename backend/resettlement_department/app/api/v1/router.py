d3212fd9f37b384bf954ecff8d5ff1ffd4bd1690
Aleksandr Andreev <dreevxq@gmail.com>
2025-04-30 14:15:03 +0300

    add wave router

--- backend/resettlement_department/app/api/v1/router.py
+++ backend/resettlement_department/app/api/v1/router.py
@@ -4,6 +4,7 @@ from api.v1.endpoints.apartment import router as apartment_router
 from api.v1.endpoints.dashboard import router as dashboard_router
 from api.v1.endpoints.history import router as history_router 
 from api.v1.endpoints.rsm import router as rsm_router
+from api.v1.endpoints.wave import router as wave_router
 
 from fastapi import APIRouter
 
@@ -15,3 +16,5 @@ router.include_router(matching_router)
 router.include_router(dashboard_router)
 router.include_router(history_router)
 router.include_router(rsm_router)
+router.include_router(wave_router)
+
