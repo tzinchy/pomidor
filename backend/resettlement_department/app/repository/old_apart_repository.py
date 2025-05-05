d5e9f336226548fdac7992c75b0cc0849d79a278
Arsenii Karpov <abs2016123@gmail.com>
2025-05-05 13:45:43 +0300

    refactor set_notes

--- backend/resettlement_department/app/repository/old_apart_repository.py
+++ backend/resettlement_department/app/repository/old_apart_repository.py
@@ -410,12 +410,9 @@ class OldApartRepository:
                 await session.rollback()
                 raise error
 
-    async def set_notes(self, apart_ids: list[int], notes: str):
+    async def set_notes(self, apart_ids: list[int], notes: str, rsm_note):
         async with self.db() as session:
             try:
-                notes_list = notes.split(";")
-                rsm_note = notes_list.pop(0)
-                notes = ";".join(notes_list)
                 placeholder = ",".join(map(str, apart_ids))
                 await session.execute(
                     text(f"""
