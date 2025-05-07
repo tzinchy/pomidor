1b03b2eec530f0d8ee365ea259b8d7dbde1aba6b
Arsenii Karpov <abs2016123@gmail.com>
2025-05-07 16:12:50 +0300

    add new filter in void aparts

--- backend/resettlement_department/app/sql/recommendation/VoidAparts.sql
+++ backend/resettlement_department/app/sql/recommendation/VoidAparts.sql
@@ -7,7 +7,7 @@ WITH offered_aparts AS (
 	WHERE affair_id = :apart_id
 ),
 apart_info AS (
-    SELECT affair_id, history_id, room_count, is_queue 
+    SELECT affair_id, history_id, room_count, is_queue, category, status_id, living_area, full_living_area, total_living_area
     FROM old_apart 
     WHERE affair_id = :apart_id
 ),
@@ -26,8 +26,13 @@ ranked_apartments AS (
         na.notes, 
         na.new_apart_id,
         na.history_id,
+        ai.status_id AS old_apart_status_id,
         ai.room_count AS required_room_count,   -- 💡 и тут!
-        ai.is_queue
+        ai.is_queue,
+        ai.category,
+        ai.living_area AS required_living_area,
+        ai.full_living_area AS required_full_living_area,
+        ai.total_living_area AS required_total_living_area
     FROM new_apart na
     CROSS JOIN apart_info ai
     WHERE na.status_id = 11
@@ -50,8 +55,11 @@ SELECT
     required_room_count,
     is_queue
 FROM ranked_apartments
-WHERE 
-    CASE 
-        WHEN is_queue = 1 THEN TRUE
-        ELSE room_count = required_room_count
-    END;
+WHERE
+	CASE
+		WHEN is_queue = 1 THEN TRUE
+		WHEN category = 70 AND is_queue = 0 THEN living_area >= required_living_area
+            AND full_living_area >= required_full_living_area
+            AND total_living_area >= required_total_living_area
+        ELSE TRUE
+	END;
