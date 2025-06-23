5df6cac34db5237fff5592ae05e88fc97bdcab29
Iosif-25IS <n.ngrp05@gmail.com>
2025-06-23 16:56:23 +0300

    last work

--- frontend/resettlement_department/src/Navigation/LinkButton/NavSvg.jsx
+++ frontend/resettlement_department/src/Navigation/LinkButton/NavSvg.jsx
@@ -34,7 +34,14 @@ export function NavSvg(state) {
           <path d="M20 30H20.0167" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           <path d="M13.3334 30H13.35" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>          
-        )
+        ),
+        'cin': (<svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.06086 13.0406L1.38773 15.9794C1.31657 16.0363 1.27515 16.1225 1.27515 16.2137L1.27515 21.045C1.27515 21.2107 1.40946 21.345 1.57515 21.345H8.92141C9.0871 21.345 9.22141 21.2107 9.22141 21.045V16.2137C9.22141 16.1225 9.17999 16.0363 9.10884 15.9794L5.4357 13.0406C5.32613 12.9529 5.17043 12.9529 5.06086 13.0406Z" stroke="currentColor" strokeWidth="2"/>
+          <path d="M26.4686 29.6998V9.10762C26.4686 8.94193 26.3343 8.80762 26.1686 8.80762H18.6124C18.4467 8.80762 18.3124 8.94193 18.3124 9.10762V29.6998C18.3124 29.8655 18.4467 29.9998 18.6124 29.9998H26.1686C26.3343 29.9998 26.4686 29.8655 26.4686 29.6998Z" stroke="currentColor" strokeWidth="2"/>
+          <path d="M26.4688 29.6998V14.6845C26.4688 14.5188 26.6031 14.3845 26.7688 14.3845H30.7C30.8657 14.3845 31 14.5188 31 14.6845V29.6998C31 29.8655 30.8657 29.9998 30.7 29.9998H26.7688C26.6031 29.9998 26.4688 29.8655 26.4688 29.6998Z" stroke="currentColor" strokeWidth="2"/>
+          <path d="M5.17175 13.2692V1H22.3904V8.80766" stroke="currentColor" strokeWidth="2"/>
+          </svg>
+        ),
     };
     return svg[state['state']]
   }
