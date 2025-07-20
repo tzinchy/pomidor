338e5794d2bc3cb5d3f2474a5cc96a1ae45e8147
Aleksandr Andreev <dreevxq@gmail.com>
2025-07-20 21:07:12 +0300

    start writing mock-router

---
+++ backend/resettlement_department/app/api/v1/endpoints/mock_oath.py
@@ -0,0 +1,10 @@
+# from fastapi import Cookie
+# from fastapi import APIRouter, Body, Response
+
+# router = APIRouter('mock_oath', prefix=['mock-oath'])
+
+# @router.post('mock_oath')
+# async def mock_oauth(type : int = Body(...), respone = Response):
+#     if type == 1: 
+#         respone.set_cookie()
+
